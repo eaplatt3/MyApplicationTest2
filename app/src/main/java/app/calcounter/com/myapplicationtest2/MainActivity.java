1387ee44513ea45a4fc83dd1eaf0464633304310
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-02-17 19:34:27 -0500

    EWRrgtadtbrdsd

--- app/src/main/java/app/calcounter/com/myapplicationtest2/MainActivity.java
+++ app/src/main/java/app/calcounter/com/myapplicationtest2/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ////hfb hedgVAHGDVHGAVSDJGVAsjhvasgh
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
